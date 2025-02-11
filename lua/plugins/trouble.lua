2152466fc0b46d84965b1295a6c9dc782497868c
Zhibin Li <haoyouab@gmail.com>
2025-02-11 19:33:38 +0800

    add trouble nvim configs

---
+++ lua/plugins/trouble.lua
@@ -0,0 +1,14 @@
+return {
+    'folke/trouble.nvim',
+    cmd = 'Trouble',
+    opts = {
+        modes = {
+            lsp = {
+                win = {
+                    type = 'float',
+                    position = 'bottom',
+                },
+            },
+        },
+    },
+}
