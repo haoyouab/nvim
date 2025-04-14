c64a0d562fc955569681f7ab1ba610f72f2aa3c5
Zhibin Li <banlu.lzb@antgroup.com>
2025-04-14 14:19:11 +0800

    update lualune layout

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -47,33 +47,18 @@ return {
             },
         },
         inactive_sections = {
-            lualine_a = {},
+            lualine_a = {
+                {
+                    'mode',
+                    separator = { left = '' },
+                    right_padding = 2,
+                },
+            },
             lualine_b = {},
             lualine_c = {
                 {
                     'filename',
                     path = 1,
-                },
-                {
-                    'aerial',
-                    -- The separator to be used to separate symbols in status line.
-                    sep = ' ) ',
-
-                    -- The number of symbols to render top-down. In order to render only 'N' last
-                    -- symbols, negative numbers may be supplied. For instance, 'depth = -1' can
-                    -- be used in order to render only current symbol.
-                    depth = nil,
-
-                    -- When 'dense' mode is on, icons are not rendered near their symbols. Only
-                    -- a single icon that represents the kind of current symbol is rendered at
-                    -- the beginning of status line.
-                    dense = false,
-
-                    -- The separator to be used to separate symbols in dense mode.
-                    dense_sep = '.',
-
-                    -- Color the symbol icons.
-                    colored = true,
                 },
             },
             lualine_x = { 'location' },
@@ -147,7 +132,33 @@ return {
         inactive_winbar = {
             lualine_a = {},
             lualine_b = {},
-            lualine_c = {},
+            lualine_c = {
+                {
+                    'filename',
+                    path = 1,
+                },
+                {
+                    'aerial',
+                    -- The separator to be used to separate symbols in status line.
+                    sep = ' ) ',
+
+                    -- The number of symbols to render top-down. In order to render only 'N' last
+                    -- symbols, negative numbers may be supplied. For instance, 'depth = -1' can
+                    -- be used in order to render only current symbol.
+                    depth = nil,
+
+                    -- When 'dense' mode is on, icons are not rendered near their symbols. Only
+                    -- a single icon that represents the kind of current symbol is rendered at
+                    -- the beginning of status line.
+                    dense = false,
+
+                    -- The separator to be used to separate symbols in dense mode.
+                    dense_sep = '.',
+
+                    -- Color the symbol icons.
+                    colored = true,
+                },
+            },
             lualine_x = {},
             lualine_y = {},
             lualine_z = {},
