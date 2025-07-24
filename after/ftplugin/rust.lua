83525cbbde340c8927662796b2b76de402e2f3a8
Zhibin Li <banlu.lzb@antgroup.com>
2025-07-24 14:01:23 +0800

    update rust config
    Signed-off-by: Zhibin Li <banlu.lzb@antgroup.com>

--- after/ftplugin/rust.lua
+++ after/ftplugin/rust.lua
@@ -1,24 +1,42 @@
 local bufnr = vim.api.nvim_get_current_buf()
-vim.keymap.set("n", "<leader>a",
-  function()
+vim.keymap.set('n', '<leader>a', function()
     vim.cmd.RustLsp('codeAction') -- supports rust-analyzer's grouping
     -- or vim.lsp.buf.codeAction() if you don't want grouping.
-  end,
-  { desc = "codeAction", silent = true, buffer = bufnr }
+end, { desc = 'codeAction', silent = true, buffer = bufnr })
+vim.keymap.set(
+    'n',
+    'K', -- Override Neovim's built-in hover keymap with rustaceanvim's hover actions
+    function()
+        vim.cmd.RustLsp({ 'hover', 'actions' })
+    end,
+    { desc = 'hoverAction', silent = true, buffer = bufnr }
 )
 vim.keymap.set(
-  "n", 
-  "K",  -- Override Neovim's built-in hover keymap with rustaceanvim's hover actions
-  function()
-    vim.cmd.RustLsp({'hover', 'actions'})
-  end,
-  { desc = "hoverAction", silent = true, buffer = bufnr }
+    'n',
+    '<leader>m', -- Override Neovim's built-in hover keymap with rustaceanvim's hover actions
+    function()
+        vim.cmd.RustLsp({ 'expandMacro' })
+    end,
+    { desc = 'expandMacro', silent = true, buffer = bufnr }
 )
-vim.keymap.set(
-  "n",
-  "<leader>m",  -- Override Neovim's built-in hover keymap with rustaceanvim's hover actions
-  function()
-    vim.cmd.RustLsp({'expandMacro'})
-  end,
-  { desc = "expandMacro", silent = true, buffer = bufnr }
-)
+
+vim.g.rustaceanvim = {
+    server = {
+        on_attach = function(client, bufnr)
+            -- you can also put keymaps in here
+        end,
+        settings = {
+            -- to enable rust-analyzer settings visit:
+            -- https://github.com/rust-analyzer/rust-analyzer/blob/master/docs/user/generated_config.adoc
+            ['rust-analyzer'] = {
+                -- enable clippy on save
+                -- checkOnSave = {
+                --     command = 'clippy',
+                -- },
+                -- cargo = {
+                --     allFeatures = true
+                -- },
+            },
+        },
+    },
+}
