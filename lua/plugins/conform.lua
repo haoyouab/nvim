24fab4754545e4b9f56fd65dde46cbeea9f784b8
Zhibin Li <banlu.lzb@antgroup.com>
2026-02-09 17:28:33 +0800

    conform.lua: use nightly rustfmt
    Signed-off-by: Zhibin Li <banlu.lzb@antgroup.com>

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,31 +1,37 @@
 return {
-  "stevearc/conform.nvim",
-  keys = {
-    {
-      "<leader>cf",
-      function()
-        require("conform").format({ lsp_fallback = true, timeout_ms = 2000 })
-      end,
-      mode = { "n", "v" },
-      desc = "Format (Conform)",
+    'stevearc/conform.nvim',
+    keys = {
+        {
+            '<leader>cf',
+            function()
+                require('conform').format({ lsp_fallback = true, timeout_ms = 2000 })
+            end,
+            mode = { 'n', 'v' },
+            desc = 'Format (Conform)',
+        },
     },
-  },
-  opts = {
-    formatters_by_ft = {
-      html = { "prettier" },
-      css = { "prettier" },
-      htmldjango = { "prettier" },
-      javascript = { "prettier" },
-      javascriptreact = { "prettier" }, -- optional
-      typescript = { "prettier" },      -- optional
-      typescriptreact = { "prettier" }, -- optional
-      python = { "black" },
-      sh = { "shfmt" },
-      bash = { "shfmt" },
-      rust = { "rustfmt" },
-      c = { "clang-format" },
-      cpp = { "clang-format" },
+    opts = {
+        formatters_by_ft = {
+            html = { 'prettier' },
+            css = { 'prettier' },
+            htmldjango = { 'prettier' },
+            javascript = { 'prettier' },
+            javascriptreact = { 'prettier' }, -- optional
+            typescript = { 'prettier' }, -- optional
+            typescriptreact = { 'prettier' }, -- optional
+            python = { 'black' },
+            sh = { 'shfmt' },
+            bash = { 'shfmt' },
+            rust = { 'rustfmt' },
+            c = { 'clang-format' },
+            cpp = { 'clang-format' },
+        },
+        formatters = {
+            rustfmt = {
+                command = 'rustup',
+                args = { 'run', 'nightly', 'rustfmt', '--edition', '2024' },
+                stdin = true,
+            },
+        },
     },
-  },
 }
-
