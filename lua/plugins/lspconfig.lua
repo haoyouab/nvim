760c03e83776f8bfa5eeea65eb53e0c8c156f984
Zhibin Li <banlu.lzb@antgroup.com>
2026-04-24 11:48:50 +0800

    update lspconfig signature
    Signed-off-by: Zhibin Li <banlu.lzb@antgroup.com>

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -2,22 +2,19 @@ return {
     'neovim/nvim-lspconfig',
     opts = {
         servers = {
-            -- Ensure mason installs the server
             clangd = {
-                root_dir = function(fname)
-                    return require('lspconfig.util').root_pattern(
-                        'Makefile',
-                        'configure.ac',
-                        'configure.in',
-                        'config.h.in',
-                        'meson.build',
-                        'meson_options.txt',
-                        'build.ninja'
-                    )(fname) or require('lspconfig.util').root_pattern(
-                        'compile_commands.json',
-                        'compile_flags.txt'
-                    )(fname) or require('lspconfig.util').find_git_ancestor(fname)
-                end,
+                root_markers = {
+                    'Makefile',
+                    'configure.ac',
+                    'configure.in',
+                    'config.h.in',
+                    'meson.build',
+                    'meson_options.txt',
+                    'build.ninja',
+                    'compile_commands.json',
+                    'compile_flags.txt',
+                    '.git',
+                },
                 capabilities = {
                     offsetEncoding = { 'utf-16' },
                 },
