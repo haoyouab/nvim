01f92a467c72610dec91a40ff14d1b9894793030
Zhibin Li <banlu.lzb@antgroup.com>
2026-04-24 16:53:20 +0800

    exit nvim if no editing window left
    Signed-off-by: Zhibin Li <banlu.lzb@antgroup.com>

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -7,11 +7,23 @@
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
 
+local sidebar_ft = { 'aerial', 'neo-tree', 'Outline' }
+local has_opened_file = false
+
 vim.api.nvim_create_autocmd('BufEnter', {
-    group = vim.api.nvim_create_augroup('close_last_aerial', { clear = true }),
+    group = vim.api.nvim_create_augroup('quit_if_only_sidebars', { clear = true }),
     callback = function()
-        if vim.bo.filetype == 'aerial' and vim.fn.winnr('$') == 1 then
-            vim.cmd('qa')
+        local wins = vim.api.nvim_list_wins()
+        for _, win in ipairs(wins) do
+            local buf = vim.api.nvim_win_get_buf(win)
+            local ft = vim.bo[buf].filetype
+            if not vim.tbl_contains(sidebar_ft, ft) and ft ~= '' then
+                has_opened_file = true
+                return
+            end
+        end
+        if has_opened_file then
+            vim.cmd('qa!')
         end
     end,
 })
